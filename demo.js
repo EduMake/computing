63427bfe84579372a25bf67ba79e0ecfe510ffdb
Martyn Eggleton <martyn.eggleton@gmail.com>
2014-10-07 15:36:04 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,6 +1,24 @@
 function main() {
     // Write your code here
-    OUTPUT("Hello World");
-
+    var Spend = INPUT("spend");
+    var LoyaltyCard = INPUT("loyalty_card");
+    var Discount = "";
+    if(Spend > 100)
+    {
+        if(LoyaltyCard === "Yes")
+        {
+            Discount = "Discount 15%";
+        }
+        else
+        {
+            Discount = "Discount 5%";
+        }
+    }
+    else
+    {
+        
+    }
+    
+    OUTPUT(Discount);
 }
 
