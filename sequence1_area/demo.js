b4768ffbad1b685883cc0f7314b9e3e782083ab3
Martyn Eggleton <martyn.eggleton@gmail.com>
2014-10-08 13:55:41 +0100

    Update demo.js

--- sequence1_area/demo.js
+++ sequence1_area/demo.js
@@ -3,6 +3,6 @@ function main() {
     var Base = INPUT("base");
     var Height = INPUT("height");
     var Area = (Base * Height);
-    OUTPUT("Area of a rectangle " + Base + " wide and " + Height + " tall = " + Area);
+    OUTPUT("Area of a " + Base + " x " + Height + " rectangle = " + Area);
 }
 
