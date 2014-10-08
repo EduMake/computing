bd689eb7012faab386dc499914fc14be285e824d
Martyn Eggleton <martyn.eggleton@gmail.com>
2014-10-08 13:08:06 +0100

    Update demo.js

--- sequence1_area/demo.js
+++ sequence1_area/demo.js
@@ -1,6 +1,8 @@
 function main() {
     // Write your code here
-    OUTPUT("Hello World");
-
+    var Base = INPUT("Base");
+    var Height = INPUT("Height");
+    var Area = (Base * Height);
+    OUTPUT("Area of a rectangle "+Base+ " wide and "+ Height +" tall = “+ Area);
 }
 
