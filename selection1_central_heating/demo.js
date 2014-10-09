3b907c7a8c3a8711f530b1fbbb980cb22b598df6
Martyn Eggleton <martyn.eggleton@gmail.com>
2014-10-09 08:43:45 +0100

    Update demo.js

--- selection1_central_heating/demo.js
+++ selection1_central_heating/demo.js
@@ -1,24 +1,14 @@
-function main() {
-    // Write your code here
-    var Spend = INPUT("spend");
-    var LoyaltyCard = INPUT("loyalty_card");
-    var Discount = "";
-    if(Spend > 100)
+function loop() {
+    var MinTemp = INPUT("mintemp");
+    var MaxTemp = INPUT("maxtemp");
+    var AirTemp = GET("airtemp");
+    
+    if(AirTemp < MinTemp)
     {
-        if(LoyaltyCard === "Yes")
-        {
-            Discount = "Discount 15%";
-        }
-        else
-        {
-            Discount = "Discount 5%";
-        }
+        OUTPUT("boiler", "ON");
     }
     else
     {
         
     }
-    
-    OUTPUT(Discount);
 }
-
