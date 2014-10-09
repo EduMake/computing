96f585f0368788fed06362b23d29d45937834bf1
Martyn Eggleton <martyn.eggleton@gmail.com>
2014-10-09 17:37:57 +0100

    Update demo.js

--- selection2_loyalty_card/demo.js
+++ selection2_loyalty_card/demo.js
@@ -1,24 +1,16 @@
 function main() {
-    // Write your code here
     var Spend = INPUT("spend");
     var LoyaltyCard = INPUT("loyalty_card");
     var Discount = "Discount not calculated yet";
-    if(Spend > 100)
-    {
-        if(LoyaltyCard === "Yes")
-        {
+    if (Spend > 100) {
+        if (LoyaltyCard === "Yes") {
             Discount = "Discount 15%";
-        }
-        else
-        {
+        } else {
             Discount = "Discount 5%";
         }
+    } else {
+
     }
-    else
-    {
-        
-    }
-    
+
     OUTPUT(Discount);
 }
-
