e4ae191aea128551c729029ddea901c8ef9d8cc1
Martyn Eggleton <martyn.eggleton@gmail.com>
2014-10-09 08:47:28 +0100

    Update demo.js

--- sequence1_area/demo.js
+++ sequence1_area/demo.js
@@ -1,14 +1,7 @@
-function loop() {
-    var MinTemp = INPUT("mintemp");
-    var MaxTemp = INPUT("maxtemp");
-    var AirTemp = GET("airtemp");
-    
-    if(AirTemp < MinTemp)
-    {
-        OUTPUT("boiler", "ON");
-    }
-    else
-    {
-        
-    }
+-function main() {
+- // Write your code here
+- var Base = INPUT("base");
+- var Height = INPUT("height");
+- var Area = (Base * Height);
+- OUTPUT("Area of a " + Base + " x " + Height + " rectangle = " + Area);
 }
